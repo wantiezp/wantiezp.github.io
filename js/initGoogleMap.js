1cc4219dfff3735bab42e250ddd5b43d81b9c289
vdutor <dutordoirv@gmail.com>
2017-03-28 17:12:18 +0200

    fix google maps key

--- js/initGoogleMap.js
+++ js/initGoogleMap.js
@@ -1,16 +1,16 @@
 function initialize() {
-var mapCanvas = document.getElementById('map');
-var mapOptions = {
-  center: new google.maps.LatLng(51.019746, 3.655284),
-  zoom: 16,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-}
-var map = new google.maps.Map(mapCanvas, mapOptions)
+    var mapCanvas = document.getElementById('map');
+    var mapOptions = {
+        center: new google.maps.LatLng(51.019746, 3.655284),
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions)
 
-  var marker = new google.maps.Marker({
-	    position: new google.maps.LatLng(51.019746, 3.655284),
-	    map: map,
-	    title: 'Hello World!'
-		});
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(51.019746, 3.655284),
+            map: map,
+            //title: 'Hello World!'
+        });
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+//google.maps.event.addDomListener(window, 'load', initialize);
